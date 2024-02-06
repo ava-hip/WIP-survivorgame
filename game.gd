877c6a8376989f9e15cbca1c36e6cea1b1b90f08
Hippolyte <93048674+ava-hip@users.noreply.github.com>
2024-02-06 17:28:39 +0100

    Initial commit

---
+++ game.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+var score = 0
+
+
+func spawn_mob():
+	var new_mob = preload("res://mob.tscn").instantiate()
+	%PathFollow2D.progress_ratio = randf()
+	new_mob.global_position = %PathFollow2D.global_position
+	add_child(new_mob, true)
+
+func _on_timer_timeout():
+	spawn_mob()
+
+func _on_player_health_depleted():
+	GameoverInterface.visible = true
+	GameoverInterface.gameover_interface_on()
+
+
