a800bb06d7bf7ed3e0007261063d58f304c16d48
Hippolyte <93048674+ava-hip@users.noreply.github.com>
2024-02-06 20:47:56 +0100

    feat : accélération des balles en fonction du score

--- scripts/bullet.gd
+++ scripts/bullet.gd
@@ -3,13 +3,25 @@ extends Area2D
 var travelled_distance = 0
 
 func _physics_process(delta):
-	const SPEED = 2000
 	const RANGE = 1200
+	var speed = 1200
+	
+	if GameoverInterface.score < 5:
+		speed = 1200
+	elif GameoverInterface.score < 15 && GameoverInterface.score > 5:
+		speed = 1500
+	elif GameoverInterface.score < 30 && GameoverInterface.score > 15:
+		speed = 2000
+	elif GameoverInterface.score < 50 && GameoverInterface.score > 30:
+		speed = 2500
+	else: 
+		speed = 3500	
+	
 	
 	var direction = Vector2.RIGHT.rotated(rotation)
-	position += direction * SPEED * delta
+	position += direction * speed * delta
 	
-	travelled_distance += SPEED * delta
+	travelled_distance += speed * delta
 	if travelled_distance > RANGE:
 		queue_free()
 
--- scripts/game.gd
+++ scripts/game.gd
@@ -2,6 +2,11 @@ extends Node2D
 
 var score = 0
 
+#func spawn_and_destroy_tree():
+	#var new_tree = preload("res://scenes/pine_tree.tscn").instantiate()
+	#%PathFollow2D.progress_ratio = randf()
+	#new_tree.global_position = %PathFollow2D.global_position
+	#add_child(new_tree, true)	
 
 func spawn_mob():
 	var new_mob = preload("res://scenes/mob.tscn").instantiate()
