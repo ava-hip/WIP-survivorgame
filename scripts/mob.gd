3845b95c69112f1b6a2ab49863079d18e10fd72e
Hippolyte <93048674+ava-hip@users.noreply.github.com>
2024-02-07 10:00:02 +0100

    feat : musique loop + dégâts de balles progressifs

--- scripts/mob.gd
+++ scripts/mob.gd
@@ -9,16 +9,21 @@ func _ready():
 
 func _physics_process(delta):
 	var direction = global_position.direction_to(player.global_position)
-	velocity = direction * 500
+	velocity = direction * 600
 	move_and_slide()
-	
-	
 
 func take_damage():
-	health -= 1
 	%Slime.play_hurt()
 	
-	if health == 0 :
+	if GameoverInterface.score < 15:
+		health -= 1
+	elif GameoverInterface.score > 15 && GameoverInterface.score < 50:
+		health -= 2
+	else:
+		health -= 3
+	
+
+	if health <= 0 :
 		GameoverInterface.score += 1
 		print(score)
 		queue_free()
