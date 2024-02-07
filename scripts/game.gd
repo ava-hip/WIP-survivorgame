4c09e1f8faf6dd3fa24397d02bda0c07e5782cef
Hippolyte <93048674+ava-hip@users.noreply.github.com>
2024-02-07 16:01:26 +0100

    feat : arbres dynamiques

--- scripts/game.gd
+++ scripts/game.gd
@@ -1,12 +1,14 @@
 extends Node2D
 
 var score = 0
+const TREE_GROUP = "trees"
 
-#func spawn_and_destroy_tree():
-	#var new_tree = preload("res://scenes/pine_tree.tscn").instantiate()
-	#%PathFollow2D.progress_ratio = randf()
-	#new_tree.global_position = %PathFollow2D.global_position
-	#add_child(new_tree, true)
+func spawn_and_destroy_tree():
+	var new_tree = preload("res://scenes/pine_tree.tscn").instantiate()
+	%PathFollow2D.progress_ratio = randf()
+	new_tree.global_position = %PathFollow2D.global_position
+	add_child(new_tree, true)
+	new_tree.add_to_group("trees")
 
 func spawn_mob():
 	var new_mob = preload("res://scenes/mob.tscn").instantiate()
@@ -17,9 +19,16 @@ func spawn_mob():
 func _on_timer_timeout():
 	spawn_mob()
 
+func _on_trees_timeout():
+	spawn_and_destroy_tree()
+
 func _on_player_health_depleted():
 	GameoverInterface.visible = true
 	GameoverInterface.gameover_interface_on()
 	GameoverInterface.display_score()
 
 
+
+func _on_area_2d_body_exited(body):
+	if body.is_in_group(TREE_GROUP):
+		body.queue_free()
